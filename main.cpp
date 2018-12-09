3f3fb27405f4aa6e7ef8caacc8fac79dde0e47d5
國井弘一 <koichi_kunii@bita.jp>
2018-12-09 16:29:33 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
+/**
+ * ヘッダ
+ * C言語とは違って.hつけなくてもよい
+ */
 #include <iostream>
- 
+
+/**
+ * 標準名前空間
+ * coutをprintf的なやつ
+ * 末尾にstdをつけることによりstd::coutとかしてしなくてもcoutだけでよびだせるよ
+ */
 using namespace std;
  
 int main(){
